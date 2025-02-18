08bb6a087f7d2d154a912342b0059ecdcb2c145b
Tanishk Sharma <tanishk1828.becse24@chitkara.edu.in>
2025-02-18 12:33:40 +0530

    Solution to Check Leap Year

--- Check Leap Year@@6788dd63490637cfc44e1b90/code.c
+++ Check Leap Year@@6788dd63490637cfc44e1b90/code.c
@@ -10,7 +10,11 @@ int main() {
             } else {
                 printf("Not a Leap Year");
             }
-        } 
-    } 
+        } else {
+            printf("Leap Year");
+        }
+    } else {
+        printf("Not a Leap Year");
+    }
     return 0;
 }
